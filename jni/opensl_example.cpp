d08874bd96265484db6cee1a584ce10351a86a70
panzy <panzhiyong84@gmail.com>
2013-12-09 17:34:20 +0800

    fix crash via auto mkdir /sdcard/tmp

--- jni/opensl_example.cpp
+++ jni/opensl_example.cpp
@@ -34,6 +34,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <queue>
 #include <stdio.h>
 #include <inttypes.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
 #include <unistd.h>
 #include "opensl_io2.h"
 
@@ -70,6 +72,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define TAG "aec" // log tag
 
+#define DUMP_PCM true
+#if DUMP_PCM
+#define dump_audio(p,f,n) dump_audio_(p,f,n)
+#else
+#define dump_audio(p,f,n) do {} while (0);
+#endif
+
 //--------------------------------------------------------------------------------
 
 void speex_ec_open (int sampleRate, int bufsize, int totalSize);
@@ -87,8 +96,6 @@ OPENSL_STREAM  *p = NULL;
 circular_buffer *farend_buf = NULL;
 circular_buffer *nearend_buf = NULL;
 circular_buffer *echo_buf = NULL;
-
-void *aecm = NULL;
 
 int64_t t_start;
 
@@ -110,9 +117,11 @@ int64_t timestamp(int64_t base)
   return ((int64_t)t.tv_sec * 1000 + (int64_t)(t.tv_usec / 1000)) - base;
 }
 
-void dump_audio(short *frame, FILE *fd, int samps)
+void dump_audio_(short *frame, FILE *fd, int samps)
 {
-  fwrite(frame, samps, 2, fd);
+  if (fd) {
+    fwrite(frame, samps, 2, fd);
+  }
 }
 
 void start()
@@ -126,10 +135,17 @@ void start()
   if(p == NULL) return; 
 
   on = 1;
-  fd_farend = fopen("/mnt/sdcard/tmp/far.dat", "w+");
-  fd_nearend = fopen("/mnt/sdcard/tmp/near.dat", "w+");
-  fd_echo = fopen("/mnt/sdcard/tmp/echo.dat", "w+");
-  fd_send = fopen("/mnt/sdcard/tmp/send.dat", "w+");
+#if DUMP_PCM
+  int r = mkdir("/mnt/sdcard/tmp", 755);
+  if (r == 0 || r == EEXIST) {
+    fd_farend = fopen("/mnt/sdcard/tmp/far.dat", "w+");
+    fd_nearend = fopen("/mnt/sdcard/tmp/near.dat", "w+");
+    fd_echo = fopen("/mnt/sdcard/tmp/echo.dat", "w+");
+    fd_send = fopen("/mnt/sdcard/tmp/send.dat", "w+");
+  } else {
+    fd_farend = fd_nearend = fd_echo = fd_send = NULL;
+  }
+#endif
   t_start = timestamp(0);
   memset(silence, 0, FRAME_SAMPS * sizeof(short));
   __android_log_print(ANDROID_LOG_DEBUG, TAG, "start, %" PRId64 , t_start); 
@@ -236,20 +252,26 @@ void runNearendProcessing()
   }
 
   android_CloseAudioDevice(p);
+  free_circular_buffer(farend_buf);
+  free_circular_buffer(echo_buf);
+  free_circular_buffer(nearend_buf);
+#if DUMP_PCM
+  if (fd_farend)
+    fclose(fd_farend);
+  if (fd_nearend)
+    fclose(fd_nearend);
+  if (fd_echo)
+    fclose(fd_echo);
+  if (fd_send)
+    fclose(fd_send);
+  fd_farend = fd_send = fd_nearend = NULL;
+#endif
+  speex_ec_close();
 }
 
 void stop()
 {
   on = 0;
-  free_circular_buffer(farend_buf);
-  free_circular_buffer(echo_buf);
-  free_circular_buffer(nearend_buf);
-  fclose(fd_farend);
-  fclose(fd_nearend);
-  fclose(fd_echo);
-  fclose(fd_send);
-  fd_farend = fd_send = fd_nearend = NULL;
-  speex_ec_close();
 }
 
 int pull(JNIEnv *env, jshortArray buf)
