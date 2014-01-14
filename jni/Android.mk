763363e03790a487f7e3ae052e5f9c6eae1e602b
panzy <panzhiyong84@gmail.com>
2014-01-14 16:53:45 +0800

    引用 speex 库时，使用相对对径。

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,15 +7,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libspeex
-LOCAL_SRC_FILES := /home/panzy/workspace/speex-1.2rc1/android/libs/$(TARGET_ARCH_ABI)/libspeex.so
+LOCAL_SRC_FILES := ../../speex-1.2rc1/android/libs/$(TARGET_ARCH_ABI)/libspeex.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE   := opensl_example
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-	/home/panzy/workspace/webrtc \
-	/home/panzy/workspace/speex-1.2rc1/include
+	$(LOCAL_PATH)/../../speex-1.2rc1/include
 LOCAL_CFLAGS := -O3
 LOCAL_CPPFLAGS :=$(LOCAL_CFLAGS)
 ###
